aa61c71bb336831adce47269a7f3641379e7e9ca
Вадим Козлов <vadimkozlov@MacBook-Air-Vadim.local>
2021-03-04 23:42:23 +0300

    Lab2 Task2

---
+++ Lab2/Task2/Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Numerics;
+using Microsoft.VisualBasic;
+
+namespace Task2 {
+    class Program {
+        static void Main(string[] args) {
+            /*
+             *  Получить текущее время и дату в двух разных форматах
+             *  и вывести на экран количество нулей, единиц, …, девяток в их записи.
+             */
+            
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine(dateTime.ToLongDateString());
+            Console.WriteLine(dateTime.ToLongTimeString());
+            Console.WriteLine(dateTime.ToShortDateString());
+            Console.WriteLine(dateTime.ToUniversalTime());
+
+            string strDateTime = dateTime.ToUniversalTime().ToString();
+            int[] countOfDigit = new int[10];
+            foreach (var character in strDateTime) {
+                if (Char.IsDigit(character)) {
+                    countOfDigit[character - '0']++;
+                }
+            }
+
+            for (int i = 0; i < 10; i++) {
+                Console.WriteLine($"Count of '{i}' is {countOfDigit[i]}");
+            }
+        }
+    }
+}
