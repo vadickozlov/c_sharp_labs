f235ba809bbd1ad43d85a9388718ad3810aaf483
Вадим Козлов <vadimkozlov@MacBook-Air-Vadim.local>
2021-03-25 19:45:46 +0300

    Lab3 added

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using School;
+
+namespace Lab3 {
+    class Program {
+        static void Main(string[] args) {
+            Pupil pupil1 = new Pupil("Vasiliy", "Petrov");
+            Pupil pupil2 = new Pupil("Evgeniy", "Agazhelskiy");
+            Pupil pupil3 = new Pupil("Alexey", "Ivanov");
+            Pupil pupil4 = new Pupil("Boris", "Ivanov");
+            Form form1 = new Form(6, 'A');
+            form1.AddPupil(pupil1);
+            form1.AddPupil(pupil2);
+            Form form2 = new Form(5, 'B');
+            form2.AddPupil(pupil3);
+            form2.AddPupil(pupil4);
+            form2.AddPupil("Dmitriy", "Rad'kov");
+            for (int i = 0; i < form1.PupilsCount; i++) {
+                Console.WriteLine(form1[i].ToString() + " ");
+            }
+            Console.WriteLine();
+            List<Pupil> form2Pupils = form2.GetPupils();
+            foreach (var pupil in form2Pupils) {
+                Console.WriteLine(pupil.ToString() + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Maximal quantity of pupils is {Form.MaxPupilsCount}");
+        }
+    }
+}
---
+++ Lab3/Lab3/School.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace School {
+    public class Pupil : IComparable {
+        public int CompareTo(object o) {
+            Pupil? pupil = o as Pupil;
+            if (pupil == null) {
+                throw new Exception("Invalid comparing");
+            }
+            if (this.Surname.Equals(pupil.Surname)) {
+                return string.Compare(this.Name, pupil.Name);
+            }
+            else {
+                return string.Compare(this.Surname, pupil.Surname);
+            }
+        }
+        private string _name;
+        private string _surname;
+        public string Name {
+            get => _name;
+            set {
+                if (string.IsNullOrEmpty(value)) {
+                    throw new Exception("Name can't be null or empty");
+                }
+                StringBuilder newName = new StringBuilder(value.ToLower());
+                newName[0] = Char.ToUpper(value[0]);
+                _name = newName.ToString();
+            }
+        }
+        public string Surname {
+            get => _surname;
+            set {
+                if (string.IsNullOrEmpty(value)) {
+                    throw new Exception("Surname can't be null or empty");
+                }
+                StringBuilder newSurname = new StringBuilder(value.ToLower());
+                newSurname[0] = Char.ToUpper(value[0]);
+                _surname = newSurname.ToString();
+            }
+        }
+        public string ToString() {
+            return _surname + " " + _name;
+        }
+        public Pupil(string name, string surname) {
+            Name = name;
+            Surname = surname;
+        }
+    }
+    public class Form {
+        private int _number;
+        private char _letter;
+        private List<Pupil> _pupils;
+        private static int _maxPupilsCount = 0;
+        public static int MaxPupilsCount { get; private set; }
+        public char Letter {
+            set {
+                if (!char.IsLetter(value)) {
+                    throw new ArgumentException();
+                } else {
+                    _letter = value;
+                }
+            }
+            get => _letter;
+        }
+        public int Number { 
+            set {
+                if (value < 1 || value > 11) {
+                    throw new ArgumentException();
+                } else {
+                    _number = value;
+                }
+            }
+            get => _number; 
+        }
+
+        public int PupilsCount {
+            get {
+                return _pupils.Count;
+            }
+        }
+        public void AddPupil(Pupil pupil) { 
+            _pupils.Add(pupil);
+            _pupils.Sort();
+            if (_pupils.Count > MaxPupilsCount) {
+                MaxPupilsCount = _pupils.Count;
+            }
+        }
+        public void AddPupil(string name, string surname) { 
+            _pupils.Add(new Pupil(name, surname));
+            _pupils.Sort();
+            if (_pupils.Count > MaxPupilsCount) {
+                MaxPupilsCount = _pupils.Count;
+            }
+        }
+        public List<Pupil> GetPupils() {
+            return _pupils;
+        }
+
+        public Pupil this[int index] {
+            get => _pupils[index];
+            set => _pupils[index] = value;
+        }
+
+        public Form(int number, char letter) {
+            Number = number;
+            Letter = letter;
+            _pupils = new List<Pupil>();
+        }
+    }
+    public class School {
+        public static int AverageCount = 0;
+        private int Number { get; set; }
+        private string City { get; set; }
+    }
+}
